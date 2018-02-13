798f69a93b856976a0eb1d5e0fb8a9f5a8dce5aa
TheMave <marius.versteegen@gmail.com>
2018-02-13 10:27:05 +0100

    Revert "toedeledoki changed to 4"
    This reverts commit ce81cf2b39c9632ffb8f01f21310fcc5b83ee81b.

--- TestWc1/Test_A/main.cpp
+++ TestWc1/Test_A/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
-    cout << "toedeledoki 4";
+    cout << "toedeledoki 3";
     string str;
     cin>>str;
 	return 0;
